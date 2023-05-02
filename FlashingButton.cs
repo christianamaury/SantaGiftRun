1d88f183628b46c33fb4c86f3783fa90480f56d9
christianamaury <chris.amaury@hotmail.com>
2023-05-02 09:09:11 -0700

    Adding flashing lights effect to the UI buttons

---
+++ FlashingButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//To access the Button Reference
+using UnityEngine.UI; 
+
+
+public class FlashingButton : MonoBehaviour
+{
+    public Button uiButton;
+    private Image buttonImage; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
