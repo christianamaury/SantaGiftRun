c9ee5c907cb87e83eaac91b5142c4d27a1e7877c
christianamaury <chris.amaury@hotmail.com>
2023-05-15 07:12:12 -0700

    Cleaning the ClickMove script

--- ClicktoMove.cs
+++ ClicktoMove.cs
@@ -32,63 +32,23 @@ public class ClicktoMove : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //..Damage on the player
-            //StaminaBar.Instance.damagePerWalking();
-
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                //..Player animation down here bro.., :D
+                //Player animation when it start running..
                 anim.SetBool("isRunning", true);
 
                 //Foot Special Sounds..
                 AudioManager.Instance.Play("DeepFootStep");
 
-                //Stamina method call down here.. 
-
+                //Player Destination to the Vector in the Screen
                 agent.SetDestination(hit.point);
-                
-                
-
-                //Manager method class down here..
-
             }
         }
 
-        /*TESTING, Bugs #2
-
-        #if UNITY_IOS || UNITY_ANDROID
-        //..If we have at least one touch in the screen..
-        if (Input.touches.Length != 0)
-        {
-            //..Current phase of the touch..
-            if (Input.touches[0].phase == TouchPhase.Began)
-            {
-                //Stamina method call down here..
-
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(startTouch);
-
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-                {
-                    //..Playing animation for the player..
-                    anim.SetBool("isRunning", true);
-
-                    //Foot Special Sounds..
-                    AudioManager.Instance.Play("DeepFootStep");
-
-                    //..Moving the player to desired location..
-                    agent.SetDestination(hit.point);
-
-                    //..Manager Game.. Saving Score Data down here
-                }
-            }
-
-            else {return;}
-        }
-        #endif
-        */
+        
     }
 
     private void OnTriggerEnter(Collider col)
@@ -96,9 +56,10 @@ public class ClicktoMove : MonoBehaviour
         if(col.gameObject.tag == "Enemy")
 
         {
-            //Playing enemy animation..
+            //Play Enemy Attack Animation..
             EController.Instance.attackingAnimation();
-            //Damage on the player..
+
+            //Setting Damage on Enemy Health
             StaminaBar.Instance.attackingPlayer();
         }
 
@@ -108,14 +69,9 @@ public class ClicktoMove : MonoBehaviour
     {
         if(col.gameObject.tag == "Enemy")
         {
-            //..Testing aqui
+            //When Enemy exit Player collider, stop attack animation..
             EController.Instance.stoppingAttackAnimation();
         }
     }
 
-    //..Testing
-    private void OnTriggerStay(Collider col)
-    {
-        
-    }
 }
