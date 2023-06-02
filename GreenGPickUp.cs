30ddc7d91b69e912b8f83530cca2994696e1c8a1
christianamaury <chris.amaury@hotmail.com>
2023-06-02 04:07:57 -0700

    Organized GreenGPickUp script

--- GreenGPickUp.cs
+++ GreenGPickUp.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class GreenGPickUp : MonoBehaviour
 {
-
     public static GreenGPickUp Instance {get; set;}
 
     private void Awake()
     {
         Instance = this; 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+ 
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Player")
