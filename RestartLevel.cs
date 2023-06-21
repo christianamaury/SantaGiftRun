94fdbe681a9b8d986a2688bab16cbf19d22e669c
christianamaury <chris.amaury@hotmail.com>
2023-06-21 04:57:51 -0700

    Organized RestartLevel script

--- RestartLevel.cs
+++ RestartLevel.cs
@@ -8,7 +8,7 @@ public class RestartLevel : MonoBehaviour
 {
     public static RestartLevel Instance {get; set;}
 
-    //Interestial Ads Purposes..
+    //Variable for Ads Purposes.,.
     private int playerDiedNumber;
     private int playerDiedRandom;
 
@@ -22,41 +22,28 @@ public class RestartLevel : MonoBehaviour
         playerDiedNumber = 2;
         playerDiedRandom = Random.Range(1, 4);
        
-        //..Game will be active every single time we start the game
+        //Game will be active every single time we start the game;
         Time.timeScale = 1;
-
     }
 
     void Awake ()
     {
         Instance = this;
-
         //Turn off the UI Canvas when the game starts..
         restartCanvasObject.SetActive(false);
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void showingRestartMenu()
     {
         //Disabled Score Textin the Game..Mainone
         GameM.Instance.scoreCount.gameObject.SetActive(false);
 
-        //..Disabled Coins Image..
+        //Disabled Coins Image..
         CurrencySystem.Instance.coinsImageGameObject.SetActive(false);
         CurrencySystem.Instance.coinsText.gameObject.SetActive(false);
 
-        //CurrencySystem.Instance.coinsTextTransfer.gameObject.SetActive(false);
-
         //Calling Randomizer IntertitialsAds nethod.
         randomInterestialAdsShow();
-
-        //GameM.Instance.bestScoreCount.gameObject.SetActive(false);
-        //GameM.Instance.bestScoreText.gameObject.SetActive(false);
 
         //Showing Game Menu
         restartCanvasObject.SetActive(true);
@@ -64,20 +51,18 @@ public class RestartLevel : MonoBehaviour
         //Pausing game.. 
         Time.timeScale = 0;
 
-        //..Animating Game Over Text
+        //Animating Game Over Text;
         GOTextAnimator.Instance.PlayTextAnimation();
      
     }
 
     public void restartingActualLevel()
     {
-
         restartCanvasObject.SetActive(false);
         Time.timeScale = 1;
 
         //Re-loading current screen..
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
     }
 
     public void randomInterestialAdsShow()
@@ -86,9 +71,6 @@ public class RestartLevel : MonoBehaviour
         {
             //Call Interestials Ads Method..
             AdsManager.Instance.showingInterestialAds();
-
-            //Random number for the variable again..
-            //playerDiedRandom = Random.Range(1, 5);
         }
         else {return;}
     }
