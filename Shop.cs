2ba95a5779266a5454c47bfec21ebbb50ac3b120
christianamaury <chris.amaury@hotmail.com>
2023-06-26 13:01:01 -0700

    Organized the Shop script

--- Shop.cs
+++ Shop.cs
@@ -7,21 +7,18 @@ public class Shop : MonoBehaviour
 {
     public static Shop Instance { get; set; }
 
-    //..Coins TextMesh Pro Object
+    //Coins Text TextMesh Pro Object.,.
     public TextMeshProUGUI coinsText;
-    //public TextMeshProUGUI healtPotionsCount;
-    //public TextMeshProUGUI armorPotionsCount;
-    
 
-    //..Once the coin has been picked up
+    //Once the coins have been picked up;
     public int coinsPickUp = 0;
     public int currentCoinsAvailable;
 
-    //..Armor and Health Points Count..
+    //Armor and Health Points Count..
     public int healthCount = 0;
     public int armorCount = 0;
 
-    //..Referenece of the amount of coins you can buy
+    //Referenece of the amount of coins you can buy;
     public const int MINIMUM_COINS = 300;
     public const int MEDIUM_COINS = 640;
     public const int LARGE_COINS = 1150;
@@ -31,37 +28,36 @@ public class Shop : MonoBehaviour
         Instance = this;
     }
 
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        //..Getting your coins Reference..
+        //Getting your coins Reference..
         coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";
     }
 
     public void BuyHealthPotions()
     {
-
-        //..Needs to be 300, testing purposes 20
+        //Needs to be 300, testing purposes 20..
         if (PlayerPrefs.GetInt("Currency", 0) >= 300)
         {
             currentCoinsAvailable = PlayerPrefs.GetInt("Currency", 0);
 
-            //..Taking some Coins Off from it
+            //Taking some Coins Off from it;
             currentCoinsAvailable = currentCoinsAvailable - 300;
 
-            //..Setting Currency.
+            //Setting Currency.,.
             PlayerPrefs.SetInt("Currency", currentCoinsAvailable);
 
             CurrencySystem.Instance.healthCount = PlayerPrefs.GetInt("HealthPotions", 0);
 
-            //..Adding Health Potions..
+            //Adding Health Potions..
             CurrencySystem.Instance.healthCount = CurrencySystem.Instance.healthCount + 3;
 
-            //..Saving these Health Potions in the System
+            //Saving these Health Potions in the System;
             PlayerPrefs.SetInt("HealthPotions", CurrencySystem.Instance.healthCount);
             CurrencySystem.Instance.healthCount = PlayerPrefs.GetInt("HealthPotions");
 
-            //..Updating TextMesh Object
+            //Updating TextMesh Object;
             coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";
 
         }
@@ -73,22 +69,22 @@ public class Shop : MonoBehaviour
         {
             currentCoinsAvailable = PlayerPrefs.GetInt("Currency", 0);
 
-            //..Taking some Coins Off from it
+            //Taking some Coins Off from it
             currentCoinsAvailable = currentCoinsAvailable - 300;
 
-            //..Setting Currency.
+            //Setting Currency.
             PlayerPrefs.SetInt("Currency", currentCoinsAvailable);
 
             CurrencySystem.Instance.armorCount = PlayerPrefs.GetInt("ArmorPotions", 0);
 
-            //..Adding Armor Potions..
+            //Adding Armor Potions..
             CurrencySystem.Instance.armorCount = CurrencySystem.Instance.armorCount + 3;
 
-            //..Saving these Armor  Potions in the System
+            //Saving these Armor  Potions in the System;
             PlayerPrefs.SetInt("ArmorPotions", CurrencySystem.Instance.armorCount);
 
             CurrencySystem.Instance.armorCount = PlayerPrefs.GetInt("ArmorPotions");
-            //..Updating TextMesh Object
+            //Updating TextMesh Object
             coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";
         }
     }
@@ -96,32 +92,37 @@ public class Shop : MonoBehaviour
     public void BuyMinimumCoins()
     {
             int coinsAvailable = PlayerPrefs.GetInt("Currency", 0);
-            //rewardedCoins = (int)amount;
+        
             coinsAvailable = coinsAvailable + 340;
-            //..Availble coins now
+            //Availble coins now..
             PlayerPrefs.SetInt("Currency", coinsAvailable);
-            //..Updating Coins Text;
+
+            //Updating Coins Text;
             coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";  
     }
 
     public void BuyMediumCoins()
     {
             int coinsAvailable = PlayerPrefs.GetInt("Currency", 0);
-            //rewardedCoins = (int)amount;
+          
             coinsAvailable = coinsAvailable + 640;
-            //..Availble coins now
+
+            //Availble coins now..
             PlayerPrefs.SetInt("Currency", coinsAvailable);
-            //..Updating Coins Text;
+
+            //Updating Coins Text;
             coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";
     }
 
     public void BuyLargeCoins()
     {
             int coinsAvailable = PlayerPrefs.GetInt("Currency", 0);
-            //rewardedCoins = (int)amount;
+            
             coinsAvailable = coinsAvailable + 1150;
-            //..Availble coins now
+
+            //Availble coins now;
             PlayerPrefs.SetInt("Currency", coinsAvailable);
+
             //..Updating Coins Text;
             coinsText.text = PlayerPrefs.GetInt("Currency", 0).ToString() + "c";
     }
