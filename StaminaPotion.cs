a59d7130b7041ff937d6cf0d0d183c2e76a9a880
christianamaury <chris.amaury@hotmail.com>
2023-07-02 02:01:58 -0700

    Organized StaminaPotion script

--- StaminaPotion.cs
+++ StaminaPotion.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class StaminaPotion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
@@ -27,14 +15,12 @@ public class StaminaPotion : MonoBehaviour
             CurrencySystem.Instance.armorCount = CurrencySystem.Instance.armorCount + 1;
             CurrencySystem.Instance.armorPotionsTextCount.text = CurrencySystem.Instance.armorCount.ToString();
 
-            //CurrencySystem.Instance.armor.text = CurrencySystem.Instance.healthCount.ToString();
-
             if (CurrencySystem.Instance.armorCount > PlayerPrefs.GetInt("ArmorPotions", 0))
             {
-                //..Setting up Potions Available
+                //Setting up Potions Available;
                 PlayerPrefs.SetInt("ArmorPotions", CurrencySystem.Instance.armorCount);
 
-                //..Updating Text Count
+                //Updating Text Count..
                 CurrencySystem.Instance.armorPotionsTextCount.text = PlayerPrefs.GetInt("ArmorPotions", 0).ToString();
 
             }
