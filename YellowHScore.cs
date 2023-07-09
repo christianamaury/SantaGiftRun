756a394b4693260163474d2fd312f3372cab39d6
christianamaury <chris.amaury@hotmail.com>
2023-07-09 08:21:45 -0700

    Organized YellowHScore script

--- YellowHScore.cs
+++ YellowHScore.cs
@@ -11,18 +11,6 @@ public class YellowHScore : MonoBehaviour
         Instance = this;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerStay(Collider col)
     {
         if(col.gameObject.tag == "Player")
@@ -30,25 +18,26 @@ public class YellowHScore : MonoBehaviour
               if (GameM.Instance.yellowGiftSCounts > 0)
 
              {
-                //..Honoring life on the player
+                //..Granting health to the Player;
                 StaminaBar.Instance.GrantHealth();
 
-                //..Playing santa special sound
+                //Playing Santa Claus Sound;
                 AudioManager.Instance.Play("MerryChristmas");
 
-                //..Saving the Actual Score
+                //Saving the Actual Score
                 GameM.Instance.savingActualScore();
 
-                //..Updating Scoretext..
+                //Updating Scoretext..
                 GameM.Instance.scoreCount.text = GameM.Instance.score.ToString();
 
                 GameM.Instance.yellowGiftSCounts = GameM.Instance.yellowGiftSCounts - 1;
-                //..This will fix UI Text Count Bug;
+
                 CurrencySystem.Instance.healthCount = PlayerPrefs.GetInt("HealthPotions", 0);
                 CurrencySystem.Instance.healthCount += 1;
                 PlayerPrefs.SetInt("HealthPotions", CurrencySystem.Instance.healthCount);
                 CurrencySystem.Instance.healthPotionsCount.text = PlayerPrefs.GetInt("HealthPotions", 0).ToString();
 
+                //..Updating Text TextMesh Object;
                 GameM.Instance.yellowGiftCount.text = GameM.Instance.yellowGiftSCounts.ToString();
                
             }
